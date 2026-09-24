7cfa3e5f591bfb3117e756a1ea4fbf12bbada2f4
Gaurav Pathak <gauravpathak128@gmail.com>
2026-09-24 15:06:52 +0530

    Added redirect feature

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,7 +1,11 @@
 function Post({ post }) {
     return (
         <div className="subCard">
-            <h3>{post.title}</h3>
+            <h3>
+                <a href={`https://www.reddit.com${post.permalink}`} target="_blank" rel="noopener noreferrer">
+                    {post.title}
+                </a>
+            </h3>
             <p>{post.selftext}</p>
             <p>⬆️ {post.ups}</p>
         </div>
